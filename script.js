2f186830252a9c3c03e215da40bfa9e04839cdd7
kavinthangavel245 <kavinthangavel245@gmail.com>
2025-10-02 12:13:09 +0530

    adds new button

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function showMessage() {
   alert("You just practiced Git! 🚀");
 }
+
+// 🌙 Dark Mode Toggle
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleButton = document.getElementById("toggle-dark");
+
+  toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+  });
+});
